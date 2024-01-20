3adae51e75f938c7d803265211efaccac0e4ce0e
Isla Waters <info@mrsdizzie.com>
2024-01-20 12:04:43 -0500

    Pull updates from tree-sitter-mojo-template

--- perl-tree-sitter.novaextension/CHANGELOG.md
+++ perl-tree-sitter.novaextension/CHANGELOG.md
@@ -1,3 +1,7 @@
+## 1.0.7
+
+Updats for tree-sitter-mojo-template
+
 ## 1.0.6
 
 Upstream update for perl
--- perl-tree-sitter.novaextension/queries/html+ep/injections.scm
+++ perl-tree-sitter.novaextension/queries/html+ep/injections.scm
@@ -6,44 +6,44 @@
 
 (
   (shorthand_perl_code
-	(perl_code) @injection.content
-	(#set! injection.language "perl+treesitter")
-	(#set! injection.combined)
+  (short_perl_code) @injection.content
+  (#set! injection.language "perl+treesitter")
+  (#set! injection.combined)
   )
 )
-	
+  
 (
   (shorthand_perl_expression
-	(perl_code) @injection.content
-	(#set! injection.language "perl+treesitter")
+  (short_perl_code) @injection.content
+  (#set! injection.language "perl+treesitter")
   )
 )
-		
+    
 (
   (shorthand_perl_expression_escaped
-	(perl_code) @injection.content
-	(#set! injection.language "perl+treesitter")
+  (short_perl_code) @injection.content
+  (#set! injection.language "perl+treesitter")
   )
 )
 
 (
   (bracketed_perl_code
-	(perl_code) @injection.content
-	(#set! injection.language "perl+treesitter")
-	(#set! injection.combined)
+  (perl_code) @injection.content
+  (#set! injection.language "perl+treesitter")
+  (#set! injection.combined)
   )
 )
-	  
+    
 (
   (bracketed_perl_expression
-	(perl_code) @injection.content
-	(#set! injection.language "perl+treesitter")
+  (perl_code) @injection.content
+  (#set! injection.language "perl+treesitter")
   )
 )
 
 (
   (bracketed_perl_expression_escaped
-	(perl_code) @injection.content
-	(#set! injection.language "perl+treesitter")
+  (perl_code) @injection.content
+  (#set! injection.language "perl+treesitter")
   )
 )
